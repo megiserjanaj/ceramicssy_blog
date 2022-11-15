bbcf9577098c46a8cacbac507062527f69fac37f
mserjanaj-klick <mserjanaj@klick.com>
2022-11-15 09:44:49 +0100

    Styling the error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,53 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
+const errorPage = {
+  marging: "2rem auto",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  textAlign: "center",
+};
+
+const errorPageh1 = {
+  margin: "0",
+  fontSize: "8rem",
+};
+
+const errorPageh2 = {
+  margin: "0",
+  fontSize: "2rem",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+};
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const errorPageP = {
+  marginTop: "2rem",
+};
+
+const errorPageA = {
+  color: "#cf1616",
+  fontWeight: "600",
+  textDecoration: "none",
+};
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
+    <div className="error-page" style={errorPage}>
+      <h1 style={errorPageh1}>404</h1>
+      <h2 style={errorPageh2}>Page not found</h2>
+      <p style={errorPageP}>
+        Sorry 😔, we couldn't find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
+        {process.env.NODE_ENV === "development" ? <></> : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/" style={errorPageA}>
+          Go home&nbsp;&rarr;
+        </Link>
       </p>
-    </main>
-  )
-}
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
